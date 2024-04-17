d2cb27f634fd4f27d6e71ff02050ac1b8ba0705a
Jorge Luiz Plautz <jorge.plautz@gmail.com>
2024-04-17 15:50:55 -0300

    build(issue#2): Criado app paciente e implentados metodos das outras apps.

--- healing/medicos/urls.py
+++ healing/medicos/urls.py
@@ -1,7 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path('cadastro_medico/', views.cadastro_medico, name='cadastro_medico'),
-]
+    path('abrir_horario/', views.abrir_horario, name='abrir_horario'),
+    path('consultas_medico/', views.consultas_medico, name='consultas_medico'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- healing/urls.py
+++ healing/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('usuarios/', include('healing.usuarios.urls')),
     path('medicos/', include('healing.medicos.urls')),
+    path('paciente/', include('healing.paciente.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
